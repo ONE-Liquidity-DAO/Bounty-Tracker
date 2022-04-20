835d32ab414750baa65c963977acec528adec7dd
leastchaos <leastchaos@gmail.com>
2022-04-20 22:48:18 +0000

    update

--- tracker/script/get_campaign_id_result.py
+++ tracker/script/get_campaign_id_result.py
@@ -1,19 +1,25 @@
-import pandas as pd
-from tracker.bounty.bounty import BountyInfo, get_bounty_info_from_campaign_id
+'''get result of campaign id'''
+from tracker.bounty.bounty import get_bounty_info_from_campaign_id
 from tracker.core.gsheet import GSheet
 from tracker.core.logger import setup_logging
 from tracker.account.account_validator import get_validated_account_infos
 from tracker.connector.ccxt.get_config import CCXTConfig
 from tracker.connector.ccxt.fetch_trades import TradeFetcher
+
+
 class MockDataBase:
     '''mock database'''
+
     def commit_task_list_to_sql(self, tasks: list) -> None:
         '''commit list of tasks to database'''
-        result = 0
+        result = {}
         for sql_class in tasks:
+            if sql_class.display_name not in result:
+                result[sql_class.display_name] = 0
             if sql_class.takerOrMaker == 'maker':
-                result += sql_class.amount
-        print(sql_class.display_name, result)
+                result[sql_class.display_name] += sql_class.amount
+        print(result)
+
 
 async def main():
     '''main script'''
